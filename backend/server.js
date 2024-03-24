72240510f67485d3ca9138b15703df845d55f454
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-03-24 18:37:12 +0000

    fix: fixing cors error

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const colors = require('colors')
-const dotenv  = require('dotenv').config();
-const {errorHandler} = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db')
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
 
-connectDB()
+connectDB();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended :false }));
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/authors', require('./routes/authorRoutes'))
+app.use("/api/authors", require("./routes/authorRoutes"));
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
