e42d23aa86a66c42f8267a709ca61e1762862e31
Mani Carmel <manicarmelvalguena@gmail.com>
2024-03-24 15:50:54 +0000

    feat : implementation of the controllers methods, they will handle the requests

--- backend/controllers/authorController.js
+++ backend/controllers/authorController.js
@@ -1,35 +1,60 @@
 const asyncHandler = require('express-async-handler')
 
+const Author = require('../models/authorModel')
+
 //@desc Get authors
 // @route GET /api/authors
 //@ access private
 const getAuthors = asyncHandler(async (req, res) => {
-    res.json({ message: 'Get authors'})
+    const authors  = await Author.find().sort({fullName :1})
+    res.json(authors)
 })
 
 //@desc Set Author
 // @route POST /api/authors
 //@ access private
 const SetAuthor = asyncHandler(async (req, res) => {
-    if(!req.body.text){
+  if(!req.body.fullName && !req.body.year_Born && !req.body.technology){
         res.status(400);
-        throw new Error('Please add a text file');
+        throw new Error('Please add the required fields');
     }
-    res.json({ message: 'Set author'})
+    const author = await Author.create({
+        fullName : req.body.fullName ,
+        year_Born : req.body.year_Born,
+        technology : req.body.technology
+    });
+
+    res.json(author)
 })
 
 //@desc update Author
 // @route UPDATE /api/authors/:id
 //@ access private
 const updateAuthor = asyncHandler(async (req, res) => {
-    res.json({ message: `Update author ${req.params.id}`})
+    const author = await Author.findById(req.params.id);
+    if(!author){
+     throw new Error('Author not found');
+    }
+
+    const updatedauthor = await Author.findByIdAndUpdate(req.params.id,
+    req.body,{
+        new : true
+    })
+
+    res.json(updatedauthor)
 })
 
 //@desc delete Author
 // @route DELETE /api/authors/:id
 //@ access private
 const deleteAuthor = asyncHandler(async (req, res) => {
-    res.json({ message: `Delete author ${req.params.id}`})
+    const author = await Author.findById(req.params.id);
+    if(!author){
+        throw new Error('Author not found');
+    }
+    const removedauthor = await Author.deleteOne(author);
+
+    res.json(removedauthor)
 })
 
 module.exports = {
---
+++ backend/models/authorModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const authorSchema = mongoose.Schema({
+    fullName : {
+        type: String,
+        required : true
+    },
+    year_Born : {
+        type: Number,
+        required : true
+
+    },
+    technology : {
+        type : String ,
+        required : true 
+    }
+})
+
+module.exports = mongoose.model('Author', authorSchema)
