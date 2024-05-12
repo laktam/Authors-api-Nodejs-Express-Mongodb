f144b8f7d4186aeaf10b89f23446e48a60f59729
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-05-12 16:30:45 +0100

    add profile to SetAuthor method

--- backend/controllers/authorController.js
+++ backend/controllers/authorController.js
@@ -1,65 +1,73 @@
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require("express-async-handler");
 
-const Author = require('../models/authorModel')
+const Author = require("../models/authorModel");
 
 //@desc Get authors
 // @route GET /api/authors
 //@ access private
 const getAuthors = asyncHandler(async (req, res) => {
-    const authors  = await Author.find().sort({fullName :1})
-    res.json(authors)
-})
+  //
+  //
+  //trying promise
+  //   await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  const authors = await Author.find().sort({ fullName: 1 });
+  res.json(authors);
+});
 
 //@desc Set Author
 // @route POST /api/authors
 //@ access private
 const SetAuthor = asyncHandler(async (req, res) => {
-  if(!req.body.fullName && !req.body.year_Born && !req.body.technology){
-        res.status(400);
-        throw new Error('Please add the required fields');
-    }
-    const author = await Author.create({
-        fullName : req.body.fullName ,
-        year_Born : req.body.year_Born,
-        technology : req.body.technology
-    });
+  if (!req.body.fullName && !req.body.year_Born && !req.body.technology) {
+    res.status(400);
+    throw new Error("Please add the required fields");
+  }
+  const author = await Author.create({
+    fullName: req.body.fullName,
+    year_Born: req.body.year_Born,
+    technology: req.body.technology,
+    profile: req.body.profile,
+  });
 
-    res.json(author)
-})
+  res.json(author);
+});
 
 //@desc update Author
 // @route UPDATE /api/authors/:id
 //@ access private
 const updateAuthor = asyncHandler(async (req, res) => {
-    const author = await Author.findById(req.params.id);
-    if(!author){
-     throw new Error('Author not found');
+  const author = await Author.findById(req.params.id);
+  if (!author) {
+    throw new Error("Author not found");
+  }
+
+  const updatedauthor = await Author.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
     }
+  );
 
-    const updatedauthor = await Author.findByIdAndUpdate(req.params.id,
-    req.body,{
-        new : true
-    })
-
-    res.json(updatedauthor)
-})
+  res.json(updatedauthor);
+});
 
 //@desc delete Author
 // @route DELETE /api/authors/:id
 //@ access private
 const deleteAuthor = asyncHandler(async (req, res) => {
-    const author = await Author.findById(req.params.id);
-    if(!author){
-        throw new Error('Author not found');
-    }
-    const removedauthor = await Author.deleteOne(author);
-
-    res.json(removedauthor)
-})
+  const author = await Author.findById(req.params.id);
+  if (!author) {
+    throw new Error("Author not found");
+  }
+  const removedauthor = await Author.deleteOne(author);
+  res.json(removedauthor);
+});
 
 module.exports = {
-    getAuthors,
-    SetAuthor,
-    updateAuthor,
-    deleteAuthor
-}
+  getAuthors,
+  SetAuthor,
+  updateAuthor,
+  deleteAuthor,
+};
--- backend/models/authorModel.js
+++ backend/models/authorModel.js
@@ -1,19 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const authorSchema = mongoose.Schema({
-    fullName : {
-        type: String,
-        required : true
-    },
-    year_Born : {
-        type: Number,
-        required : true
+  fullName: {
+    type: String,
+    required: true,
+  },
+  year_Born: {
+    type: Number,
+    required: true,
+  },
+  technology: {
+    type: String,
+    required: true,
+  },
+  profile: {
+    type: String,
+    required: true,
+  },
+});
 
-    },
-    technology : {
-        type : String ,
-        required : true 
-    }
-})
-
-module.exports = mongoose.model('Author', authorSchema)
+module.exports = mongoose.model("Author", authorSchema);
